8f4cc95008ba9afd84c8b5001b50959edb623506
lafanx3 <x3bgla@hotmail.com>
2016-03-01 13:47:06 -0800

    Create EnrollmentStatusEnum.cs

---
+++ EnrollmentStatusEnum.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Datalus.Web.Enums
+{
+    public enum EnrollmentStatus
+    {
+        Requested=1,
+        Approved=2,
+        Waitlisted=3,
+        Enrolled=4,
+        Withdrawn=5,
+        Completed=6
+    }
+}
