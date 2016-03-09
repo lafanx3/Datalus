3838853c83efe22ca0710c3e6d25113a4261f412
lafanx3 <bennygeorge89@gmail.com>
2016-03-09 14:23:22 -0800

    Create AngularModalSection.js

---
+++ Views/UserSection/AngularModalSection.js
@@ -0,0 +1,49 @@
+        //Modal Section controller
+        (function () {
+            "use strict";
+
+            angular.module(APPNAME)
+                .controller('modalSectionController', ModalSectionController);
+
+            //  $uibModalInstance is coming from the UI Bootstrap library and is a reference to the modal window itself so we can work with it
+            //  items is the array passed in from the main controller above through the resolve property
+            ModalSectionController.$inject = ['$scope', '$baseController', '$uibModalInstance', '$userSectionService', 'items']
+
+            function ModalSectionController(
+                $scope
+                , $baseController
+                , $uibModalInstance
+                , $userSectionService
+                , items) {
+
+                var vm = this;
+                vm.userData = null;
+                vm.userProfileId = null;
+                vm.sectionId = null;
+                //validation object
+                vm.mStudentRegistrationForm = null;
+                vm.showFormErrors = false;
+
+                vm.$scope = $scope;
+                vm.$uibModalInstance = $uibModalInstance;
+                vm.$userSectionService = $userSectionService;
+
+                vm.userData = items;
+
+                $baseController.merge(vm, $baseController);
+
+                //  $uibModalInstance is used to communicate and send data back to main controller
+                vm.ok = function () {
+                    vm.showFormErrors = true;
+                    if (vm.mStudentRegistrationForm.$valid) {
+                        vm.$uibModalInstance.close(vm.mStudentRegistrationForm);
+                    } else {
+                        console.log("validation failed");
+                    }
+                };
+
+                vm.cancel = function () {
+                    vm.$uibModalInstance.dismiss('cancel');
+                };
+            }
+        })();
